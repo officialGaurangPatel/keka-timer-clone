cb3136e5a89f61f3813ac4404b845515d511d329
Gaurang Patel <gaurangp.albiorix@gmail.com>
2023-05-10 12:53:23 +0530

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'keka-timer-clone';
+
+  constructor() {}
+
+  display: any;
+  center: google.maps.LatLngLiteral = {
+    lat: 24,
+    lng: 12,
+  };
+  zoom = 4;
+  moveMap(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.center = event.latLng.toJSON();
+  }
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
 }
